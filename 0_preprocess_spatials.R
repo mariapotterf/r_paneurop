fd7b9b6073de3f32096a281553b3997481fbb6f3
Maria Potterf <ge45lep@mytum.de>
2023-10-18 11:26:40 +0200

    read all gpkgs from tehfield, and split them into individual countries

--- 0_preprocess_spatials.R
+++ 0_preprocess_spatials.R
@@ -7,3 +7,55 @@
 # split shps by country names
 # export gpkgs by country
 
+library(terra)
+library(rnaturalearth)
+library(rnaturalearthdata)
+library(sf)
+
+
+setwd('C:/Users/ge45lep/Documents/2023_PanEuropean/r_paneurop/rawData/collected')
+
+
+# list all field data collected; get paths
+all_gpkg <- list.files(pattern = "*.gpkg")
+
+# load gpkgs
+all_gpkg.ls <- lapply(all_gpkg, function(name) {vect(name)})
+
+# merge them all in one file
+merged_gpkg <- do.call("rbind", all_gpkg.ls)
+
+# set as sf
+merged_gpkg <- sf::st_as_sf(merged_gpkg)
+
+
+# get shp of european countries
+ge <- ne_countries(country = "germany",     type = "countries", returnclass = 'sf', scale = 'medium')
+at <- ne_countries(country = "austria",     type = "countries", returnclass = 'sf', scale = 'medium')
+sk <- ne_countries(country = "slovakia",    type = "countries", returnclass = 'sf', scale = 'medium')
+sl <- ne_countries(country = "slovenia",    type = "countries", returnclass = 'sf', scale = 'medium')
+fr <- ne_countries(country = "france",      type = "countries", returnclass = 'sf', scale = 'medium')
+ch <- ne_countries(country = "switzerland", type = "countries", returnclass = 'sf', scale = 'medium')
+pl <- ne_countries(country = "poland",      type = "countries", returnclass = 'sf', scale = 'medium')
+cz <- ne_countries(country = "czech republic", type = "countries", returnclass = 'sf', scale = 'medium')
+it <- ne_countries(country = "italy",       type = "countries", returnclass = 'sf', scale = 'medium')
+
+# merge EU countries
+mrg_cntrs <- dplyr::bind_rows(list(ge, at, sk, sl,fr, ch, pl, cz, it))
+
+# project vector data to the same crs
+mrg_cntrs_pr <-st_transform(mrg_cntrs, crs(merged_gpkg)) 
+
+
+plot(mrg_cntrs_pr$geometry)
+plot(merged_gpkg$geometry, add = T)
+
+
+# FITS nicely!
+  # 
+
+# split the gpkgs by countries, export
+
+
+
+
