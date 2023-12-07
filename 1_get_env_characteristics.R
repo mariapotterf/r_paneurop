bbcc12b25e7f9ec5b200c3888af6c5664c3320f3
Maria Potterf <ge45lep@mytum.de>
2023-12-07 04:11:09 +0100

    create function to extract nearest distance from teh plot

--- 1_get_env_characteristics.R
+++ 1_get_env_characteristics.R
@@ -19,6 +19,8 @@
 
 
 library(terra)
+library(raster)
+
 
 # list all countries
 country_name = c('austria')
@@ -58,37 +60,27 @@ country_names <- list( "austria", "czechia", "france", "germany", "italy", "pola
 
 
 
-# test distance to the nearest edge
-library(terra)
 
 
 # distances will contain the distance of each point to the nearest edge of its patch
-
+library(sf)
 
 library(terra)
+library(raster)
+
+
 
 # Create a small example raster
-mat <- matrix(c(1, 2, 2, 2, 2,
+mat <- matrix(c(1, NA, 2, 2, 3,
+                NA, 2, 2, 2, 4,
                 2, 2, 2, 2, 2,
-                2, 2, 2, 2, 2,
-                2, 2, 2, 2,2,
+                2, 2, 2, 1, 2,
                 2, 2, 2, 2, 2), byrow = T, nrow=5, ncol=5)
 example_raster <- rast(nrows=5, ncols=5, vals=mat, crs = 'EPSG:3035')
 
 # Reclassify the raster - change values '2' to NA
-reclass_matrix <- matrix(c(1, NA), ncol=2, byrow=TRUE)
+reclass_matrix <- matrix(c(1,2, NA), ncol=3, byrow=TRUE)  #matrix(c(1, NA), ncol=2, byrow=TRUE)
 reclassified_raster <- classify(example_raster, reclass_matrix)
-
-# Calculate the distance to the nearest NA
-distance_to_na <- distance(reclassified_raster, point_vector)
-
-
-
-library(raster)
-
-rRDist <- raster::distanceFromPoints(reclassified_raster, point_vector)
-rRDist <- raster::mask(rRDist, rR)
-raster::plot(rRDist)
 
 # Assuming reclassified_raster is your raster
 # Create a data frame with the coordinates of the point
@@ -96,6 +88,10 @@ point_df <- data.frame(x = 150, y =-80)
 
 # Create a SpatVector from the data frame
 point_vector <- vect(point_df, geom = c("x", "y"), crs = crs(reclassified_raster))
+
+
+# Calculate the distance to the nearest NA
+distance_to_na <- distance(reclassified_raster) # , point_vector
 
 
 # Plotting to visualize the results
@@ -113,8 +109,68 @@ plot(point_vector, main="", add = T)
 
 
 
-country_name = 'czechia'
-# distance to edge: test for austria -----------------------------------------------------------
+
+
+
+
+
+#  example with lines
+
+library(terra)
+set.seed(123)
+
+# Create an empty raster and set all values to NA initially
+raster <- rast(ext=c(401000, 402000, 4405000, 4406000), res = 30, crs="local")
+values(raster) <- NA
+
+# Define a patch area and set these cells to 2 (covering about 1/4 of the raster)
+patch_cells <- cells(raster, ext(c(401000, 401500, 4405000, 4405500)))
+raster[patch_cells] <- 2
+
+# Create points that fall within the patch
+points <- data.frame(
+  ID = 1:4,
+  x = runif(4, 401000, 401500),  # X coordinates within the patch
+  y = runif(4, 4405000, 4405500) # Y coordinates within the patch
+)
+
+points <- data.frame(
+  ID = 1,
+  x = 401200,  # X coordinates within the patch
+  y = 4405100 # Y coordinates within the patch
+)
+
+pts <- vect(points, geom=c("x", "y"), crs=crs(raster))
+
+# Plotting to visualize the raster and points
+plot(raster, main="Raster with Patch")
+plot(lns, add = TRUE, col = 'blue', lwd = 4)
+plot(pts, add=TRUE, col="red", pch=16)
+
+
+# convert raster patches to polygons and to lines to measure the distances
+lns <- as.lines(as.polygons(raster, dissolve=TRUE))
+
+nearest(pts, lns) |> values()
+
+x <- distance(raster>0)
+extract(x, pts)
+
+
+
+
+
+
+
+
+
+
+
+
+
+# distance to edge: test for slovakia -----------------------------------------------------------
+country_name = 'slovakia'
+
 print(country_name)
 
 # read field data 
@@ -128,27 +184,32 @@ disturbance  = rast(paste(dist_path, country_name, disturb_name, sep = '/'))
 # read raster data
 desired_crs <- crs(disturbance)
 
+# Create a SpatVector from the data frame
+point_vector <- country_proj[1,]# vect(point_df, geom = c("x", "y"), crs = crs(reclassified_raster))
 
+
+# get buffer
+buff <- buffer(point_vector, 100)
+
+# crop data and then mask them to have a circle
+disturbance_crop <- crop(disturbance, buff)
+disturbance_mask <- mask(disturbance_crop, buff)
+
+plot(disturbance_mask)
 
 # Reclassify the raster - change values '2' to NA
-reclass_matrix <- matrix(c(1985, 2017.1, NA,
-                           2017.5, 2021, 1), ncol=3, byrow=TRUE)
-reclassified_raster <- classify(disturbance, reclass_matrix)
+reclass_matrix <- matrix(c(1985, 2017.1, 1,
+                           2017.5, 2021, NA), ncol=3, byrow=TRUE)
+reclassified_raster <- classify(disturbance_mask, reclass_matrix)
+
+plot(reclassified_raster)
 
 # Calculate the distance to the nearest NA
 distance_to_na <- distance(reclassified_raster)
 
 # Plotting to visualize the results
-plot(example_raster, main="example_raster")
 plot(reclassified_raster, main="Reclassified Raster")
 plot(distance_to_na, main="Distance to Nearest NA")
-
-# Assuming reclassified_raster is your raster
-# Create a data frame with the coordinates of the point
-#point_df <- country_proj#data.frame(x = 100, y =60)
-
-# Create a SpatVector from the data frame
-point_vector <- country_proj# vect(point_df, geom = c("x", "y"), crs = crs(reclassified_raster))
 
 # Measure the distance from the point to the nearest NA
 point_distance <- extract(distance_to_na, point_vector)
@@ -156,9 +217,72 @@ point_distance <- extract(distance_to_na, point_vector)
 plot(distance_to_na, main="Distance to Nearest NA")
 plot(point_vector, main="", add = T)
 
-plot(point_vector)
-plot(disturbance, add = T)
-plot(reclassified_raster, add = T, col = 'red')
+
+
+
+
+# Make a function distance ti edge -------------------------
+process_point <- function(point, disturbance, buffer_dist) {
+  # Create buffer around the point
+  buff <- buffer(point, buffer_dist)
+  
+  # Crop and mask the disturbance raster
+  disturbance_crop <- crop(disturbance, buff)
+  disturbance_mask <- mask(disturbance_crop, buff)
+  
+  # Reclassify the raster
+  reclass_matrix <- matrix(c(1985, 2017.1, 1,
+                             2017.5, 2021, NA), ncol=3, byrow=TRUE)
+  reclassified_raster <- classify(disturbance_mask, reclass_matrix)
+  
+  # Calculate the distance to the nearest NA
+  distance_to_na <- distance(reclassified_raster)
+  
+  # Extract the distance for the point
+  point_distance <- extract(distance_to_na, point)
+  
+  return(data.frame(ID = point$ID, distance = point_distance))
+}
+
+# Example usage:
+# Assuming 'disturbance' is your raster and 'country_proj' is your SpatVector
+
+results <- lapply(1:nrow(country_proj), function(i) {
+  point_vector <- country_proj[i,]
+  process_point(point_vector, disturbance, 500)
+})
+
+# Combine all results into one dataframe
+final_results <- do.call(rbind, results)
+print(final_results)
+
+
+
+
+# Assuming reclassified_raster is your raster
+# Create a data frame with the coordinates of the point
+point_df <- data.frame(x = 150, y =-80)
+
+# Create a SpatVector from the data frame
+point_vector <- vect(point_df, geom = c("x", "y"), crs = crs(reclassified_raster))
+
+
+# Calculate the distance to the nearest NA
+distance_to_na <- distance(reclassified_raster) # , point_vector
+
+
+# Plotting to visualize the results
+plot(example_raster, main="example_raster")
+plot(reclassified_raster, main="Reclassified Raster")
+plot(distance_to_na, main="")
+plot(point_vector, main="", add = T)
+
+# Measure the distance from the point to the nearest NA
+point_distance <- extract(distance_to_na, point_vector)
+(point_distance)
+plot(distance_to_na, main="Distance to Nearest NA")
+plot(point_vector, main="", add = T)
+
 
 
 
