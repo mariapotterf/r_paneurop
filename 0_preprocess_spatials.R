ef763013277e4d909aacf657d8eea2cce249c571
Maria Potterf <ge45lep@mytum.de>
2023-12-12 05:59:29 +0100

    check data

--- 0_preprocess_spatials.R
+++ 0_preprocess_spatials.R
@@ -11,6 +11,7 @@ library(terra)
 library(rnaturalearth)
 library(rnaturalearthdata)
 library(sf)
+library(data.table)
 
 
 
@@ -19,14 +20,24 @@ all_gpkg <- list.files(path = "rawData/collected",
                        pattern = "*.gpkg$", 
                        full.names = TRUE)  # read the full path to read the files properly
 
+(all_gpkg)
+
 # load gpkgs
 all_gpkg.ls <- lapply(all_gpkg, function(name) {vect(name)})
+
+
+#lapply(all_gpkg, function(name) {crs(name)})
+
+crs(all_gpkg.ls[[12]])
+
 
 # merge them all in one file
 merged_gpkg <- do.call("rbind", all_gpkg.ls)
 
 # convert to sf
 merged_gpkg <- sf::st_as_sf(merged_gpkg)
+
+lapply(merged_gpkg, function(x) {x$sovereignt})
 
 
 # get shp of european countries
@@ -60,14 +71,14 @@ data_clipped <- st_intersection(x_proj, merged_gpkg)
 data_clean <- 
   data_clipped %>% 
   dplyr::mutate(group = group + 100) %>% 
-  dplyr::mutate(ID = paste(region, group, point, sep="_"),
-                country = name ) %>%  #dplyr::select(5:323) %>% names()
+  dplyr::mutate(#country = name,
+                ID = paste(country, region, group, point, sep="_")) %>%  #dplyr::select(5:323) %>% names()
   dplyr::select(ID, country)
 
 # export as gpkg
 outName = paste0('dat_', name,  '.gpkg')
 st_write(data_clipped, paste('C:/Users/ge45lep/Documents/2023_PanEuropean/r_paneurop/outData', outName, sep = "/"),
-         layer = 'dat', append=FALSE)
+         layer = 'dat', type = 'points', append=FALSE)
 
 
 
@@ -92,14 +103,15 @@ clip_dat <- function(x, ...) {
   data_clean <- 
     data_clipped %>% 
     dplyr::mutate(group = group + 100) %>% 
-    dplyr::mutate(ID = paste(region, group, point, sep="_"),
-                  country = name ) %>%  #dplyr::select(5:323) %>% names()
+    dplyr::mutate(#country = name,
+                  ID = paste(country, region, group, point, sep="_")) %>%  #dplyr::select(5:323) %>% names()
     dplyr::select(ID, country)
+  
   
   
   # export as gpkg
   outName = paste0('dat_', name,  '.gpkg')
-  st_write(data_clean, paste('C:/Users/ge45lep/Documents/2023_PanEuropean/r_paneurop/outData', outName, sep = "/"),
+  st_write(data_clean, paste('outData', outName, sep = "/"),
            layer = 'dat', append=FALSE)
   
  
@@ -111,13 +123,4 @@ clip_dat(cntrs_ls[[1]])
 # export gpkgs by individual countries
 lapply(cntrs_ls, clip_dat)
 
-# FITS nicely!
-  # 
-st_crs(ge) == st_crs(merged_gpkg)
-# split the gpkgs by countries, export as individual gpkg per country
-ge_data <- st_intersection(mrg_cntrs_pr[[1]], merged_gpkg)
-
-
-plot(mrg_cntrs_pr$geometry)
-plot(merged_gpkg$geometry, add = T)
 
