65f4d5e1cfd69e28ea5ee9d3f626cded62118338
Maria Potterf <ge45lep@mytum.de>
2024-10-30 09:29:44 +0100

    move in the tables and calculations

--- 10_make_plots.R
+++ 10_make_plots.R
@@ -40,3 +40,33 @@ theme_set(
     )
 )
 
+
+# read data --------------------------------------------------------------------------
+
+df_fin <- fread('outTable/df_fin.csv')  # update table with moe columsn 
+
+# get vegetation data
+load("outData/veg.Rdata")
+
+
+
+# Get gables
+
+#View(df_fin)
+# get summary for the scatter plot, one point is one country & management
+df_summary <- df_fin %>%
+  # group_by(manag) %>% # country, 
+  summarize(
+    rich_med = median(rIVI, na.rm = TRUE),
+    rich_sd = sd(rIVI, na.rm = TRUE),
+    rich_25 = quantile(rIVI, 0.25, na.rm = TRUE), # rich_mean -rich_sd, #
+    rich_75 = quantile(rIVI, 0.75, na.rm = TRUE), # rich_mean +rich_sd,
+    dens_med = median(stem_density   , na.rm = TRUE),
+    dens_sd   = sd(stem_density  , na.rm = TRUE),
+    dens_25 = quantile(stem_density  , 0.25, na.rm = TRUE), # dens_mean - dens_sd, #
+    dens_75 = quantile(stem_density  , 0.75, na.rm = TRUE), # dens_mean + dens_sd, #
+    .groups = 'drop'
+  )
+
+(df_summary)
+
